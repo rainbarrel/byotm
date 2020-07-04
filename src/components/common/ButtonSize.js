c007a1de9e15d2ce81b9ad1f23acd99c3a1c1745
msantam2 <msantam2@gmail.com>
2020-07-03 22:03:50 -0700

    wip

--- src/components/common/ButtonSize.js
+++ src/components/common/ButtonSize.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 
 const ButtonSize = (props) => {
   const {
