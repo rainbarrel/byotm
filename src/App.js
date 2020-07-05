1fa9185ace29338108057c65f903195c897bd699
msantam2 <msantam2@gmail.com>
2020-07-04 19:33:39 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
     this.state = {
       recognizer: null,
 
-      waitingForConfirmation: true,
+      waitingForConfirmation: false,
 
       audioClassTriggered: null,
       phoneNumberRecipient: null,
